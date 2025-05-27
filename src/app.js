46be8bc4c4db266fcf4c2bbfcdddd6cbd42940c4
Neelakantam-Jami <neelakantamjami24@gmail.com>
2025-05-27 13:47:18 +0530

    request apis added

--- src/app.js
+++ src/app.js
@@ -7,9 +7,11 @@ app.use(cookieParser());
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 connectDB().then(() => {
     console.log("Database Connection established successfully...");
